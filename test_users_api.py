9910de3ff288c098ea93078af0e38108849bf75b
Razvan Vancea <razvan.vancea@zitec.com>
2023-10-13 15:15:08 +0300

    refactor tests

--- test_users_api.py
+++ test_users_api.py
@@ -6,7 +6,7 @@ faker = faker.Faker()
 
 base_url = "https://reqres.in"
 
-
+#test cases
 def test_get_test():
     url = base_url + "/api/users"
     response = requests.get(url)
@@ -17,11 +17,11 @@ def test_get_test():
 
 
 def test_create_user():
-    random_email = faker.email()
+    random_first_name = faker.first_name()
     url = base_url + "/api/users"
     headers = {"Content-Type": "application/json"}
     request_body = {
-        "name": random_email,
+        "name": random_first_name,
         "job": "QA"
     }
     response = requests.post(url, json=request_body, headers=headers)
@@ -30,19 +30,33 @@ def test_create_user():
     json_str = json.dumps(json_data, indent=4)
     print("POST ENDPOINT response is: ", json_str)
     assert "name" in json_data
+    assert json_data["name"] == random_first_name
     assert json_data["job"] == "QA"
-    user_id = json_data["id"]
-    return user_id
 
 
-def test_delete_user(id):
-    url = base_url + f"/api/users/{id}"
+def test_delete_user():
+    random_first_name = faker.first_name()
+    user_id = create_user(random_first_name, "DEV")
+    url = base_url + f"/api/users/{user_id}"
     response = requests.delete(url)
     assert response.status_code == 204
-    print(f" user with id {id} deleted")
+    print(f" user with id {user_id} deleted")
+    print('the random name is: ' + random_first_name)
+
+#utility functions
+def create_user(name, job):
+    url = base_url + "/api/users"
+    headers = {"Content-Type": "application/json"}
+    request_body = {
+        "name": name,
+        "job": job
+    }
+    response = requests.post(url, json=request_body, headers=headers)
+    json_data = response.json()
+    return json_data["id"]
 
 
 # call tests
-user_id = test_create_user()
-test_delete_user(user_id)
+test_create_user()
+test_delete_user()
 test_get_test()
